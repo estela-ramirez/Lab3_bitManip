9677b0e51f1b4c1f4eeb04f04deec9f5012d6689
estela-ramirez <ramirezestela01@gmail.com>
2021-01-15 15:41:27 -0800

    first version of part2.c file

--- source/main.c
+++ source/main.c
@@ -14,10 +14,31 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+    unsigned char tmpA = 0x00;
+    unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
     /* Insert your solution below */
     while (1) {
 
+        tmpA = PINA & 0x0F;
+        (tmpC ?  ( tmpA < 4 | 0x80  :  0x00 );
+
+        if ( tmpA ==  3 || tmpA == 4){ 
+            tmpC = tmpC | 0x030;    //00110000
+            
+        }else if ( tmpA == 5 || tmpA == 6){
+            tmpC = tmpC | 0x38;    //00111000
+        }else if ( tmpA >= 7 || tmpA <= 9){
+            tmpC = tmpC | 0x3C; //00111100
+        }else if (tmpA >= 10 || tmpA <= 12){
+            tmpC = tmpC | 0x3E;  // 00111110
+        }else if (tmpA >= 13 || tmpA <= 15){
+            tmpC = tmpC | 0x3F;  //00111111
+        }else{
+            tmpC = tmpC;
+        }
+        PORTC = tmpC;
     }
     return 1;
 }
