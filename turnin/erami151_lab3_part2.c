e5428555d7d49d80a42ca968bc88d7b615a12aa5
Estela Ramirez <ramirezestela01@gmail.com>
2021-01-15 21:06:22 -0800

    Completed part2 c file

--- turnin/erami151_lab3_part2.c
+++ turnin/erami151_lab3_part2.c
@@ -1,7 +1,7 @@
-/*	Author: erami151
+/*	Author: Estela Ramirez Ramirez
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 22
+ *	Assignment: Lab #3  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -16,30 +16,33 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
     DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-    unsigned char tmpA = 0x00;
+    unsigned char tmpA = 0x00; // Temporary variable to hold A's (3-0) bits
     unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
     /* Insert your solution below */
     while (1) {
 
         tmpA = PINA & 0x0F;
        
-        if ( tmpA ==  3 && tmpA == 4){ 
-            tmpC = tmpC | 0x30;    //00110000
-            
-        }else if ( tmpA == 5 && tmpA == 6){
-            tmpC = tmpC | 0x38;    //00111000
-        }else if ( tmpA >= 7 && tmpA <= 9){
-            tmpC = tmpC | 0x3C; //00111100
-        }else if (tmpA >= 10 && tmpA <= 12){
-            tmpC = tmpC | 0x3E;  // 00111110
-        }else if (tmpA >= 13 && tmpA <= 15){
-            tmpC = tmpC | 0x3F;  //00111111
+        if (tmpA == 0x01 || tmpA == 0x02){
+            tmpC =  0x20;
+        }else if ( tmpA ==  0x03 || tmpA == 0x04){ 
+            tmpC = 0x30;    //00110000
+        }else if ( tmpA == 0x05 || tmpA == 0x06){
+            tmpC = 0x38;    //00111000
+        }else if ( tmpA >= 0x07 && tmpA <= 0x09){
+            tmpC =  0x3C; //00111100
+        }else if (tmpA >= 0x0A && tmpA <= 0x0C){
+            tmpC =  0x3E;  // 00111110
+        }else if (tmpA >= 0x0D && tmpA <= 0x0F){
+            tmpC = 0x3F;  //00111111
         }else{
-            tmpC = tmpC;
+            tmpC = 0x00;
         }
         if (tmpA <= 4){
-            tmpC = tmpC | 0x40;  //01000000
+            tmpC = tmpC | 0x60;  //01000000
         }  // going here even if tmpA is not less than 4
+
+        
         PORTC = tmpC;
     }
     return 1;
